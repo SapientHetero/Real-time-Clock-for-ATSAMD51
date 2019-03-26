198529f9d46b298beb11c794cf18ed67633cf510
Sapient Hetero <39421987+SapientHetero@users.noreply.github.com>
2019-03-26 11:41:00 -0400

    Initial upload

---
+++ RTCexample.ino
@@ -0,0 +1,68 @@
+/* Copyright 2019 Ron Sutton
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+extern "C"{
+  #include "rtcFunctions.h"
+}
+
+void setup() {
+    //Initialize serial and wait for port to open:
+  Serial.begin(500000);
+  while (!Serial) {
+    ; // wait for serial port to connect.
+  }
+
+  rtcInit();                             // initialize RTC
+
+}
+
+void loop() {
+
+  char timeChars[12];
+  volatile uint32_t t;
+
+  /* Timing test results - How long does it take to read the RTC?
+   *  Iterations        Total Time         Average time per RTC read
+   *  ==========        ==========         =========================
+   *    10,000          1920 usecs               .19200 usecs
+   *    20,000          3839 usecs               .19195 usecs
+   *    50,000          9597 usecs               .19194 usecs
+   *    100,000        16691 usecs               .16691 usecs
+   *  1,000,000       166907 usecs               .166907 usecs
+   */
+
+  Serial.println("Starting RTC read loop");   // RTC read timing test
+  uint32_t iterations = 100000;               //  set the number of loop iterations...
+  uint32_t start = micros();                  //   record the time at the start of the loop, in microseconds...
+  for (uint32_t i = 0; i<iterations; i++) {   //    read the RTC "iterations" times...
+    t = rtcGetUnixTime();         
+  }
+  start = micros() - start;                   //     calculate loop run time in microseconds...
+  Serial.print("Aggregate time for ");
+  Serial.print(iterations);
+  Serial.print(" RTC reads = ");
+  Serial.print(start);
+  Serial.println(" useconds");  
+
+  while (1) {
+    uint32_t count = rtcGetUnixTime();  // read RTC count...
+    Serial.print("Count = ");           //  display it...
+    Serial.println(count);
+    delay(5000);                        //   and wait 5 seconds before doing it again.
+    if (Serial.available()) {           // To test function that sets RTC, user must type a char in Serial Monitor and hit Enter...
+      char c = Serial.read();           //  read and discard character
+      Serial.println("Enter new Unix epoch"); // after prompt for new Unix epoch (time in seconds since Jan 1, 1970)...
+      Serial.readBytesUntil('\r', timeChars, 11); // user must enter new epoch and hit Enter...
+      uint32_t newTime = atoi(timeChars); // time is converted from characters to a 32 bit unsigned integer...
+      rtcSetUnixTime(newTime);            //   and used to set RTC. Subsequent RTC reads should reflect the new setting.
+    }
+  }
+
+}
---
+++ rtcFunctions.c
@@ -0,0 +1,53 @@
+/* Copyright 2019 Ron Sutton
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+/*******************************************************************************************************************************************************************************************
+  ATSAMD51J19A Real-Time Clock functions
+*******************************************************************************************************************************************************************************************/
+/* These functions provide access to the features of ATSAMD51J19A Real-Time Clock peripheral */
+#pragma optimize( "", off )
+#include "Arduino.h"
+#include "rtcFunctions.h"
+
+// This function sets up and starts the TRNG
+void rtcInit(void) {
+
+  OSC32KCTRL->XOSC32K.bit.EN1K = 1;                                     // enable XOSC32K 1KHz clock output
+  OSC32KCTRL->RTCCTRL.reg = OSC32KCTRL_RTCCTRL_RTCSEL_XOSC1K;           // select 1KHz clock to drive RTC...
+
+  RTC->MODE0.DBGCTRL.reg = RTC_DBGCTRL_DBGRUN;                          // set RTC to continue running when processor is halted by debugger
+  
+  RTC->MODE0.CTRLA.reg = RTC_MODE0_CTRLA_MODE_COUNT32 |                 // put RTC in 32 bit counter mode...
+                   RTC_MODE0_CTRLA_ENABLE |                             //  enabled...
+                   RTC_MODE0_CTRLA_PRESCALER_DIV1024 |                  //   with prescaler set to 1024 to get 1 second count interval ...
+                   RTC_MODE0_CTRLA_COUNTSYNC;                           //    and COUNTSYNC set to get stable count values on reads.
+  
+  while (RTC->MODE0.SYNCBUSY.reg & (RTC_MODE0_SYNCBUSY_ENABLE // wait for RTC settings for COUNTSYNC and ENABLE to be synced
+                                      | RTC_MODE0_SYNCBUSY_COUNTSYNC) != 0) ;
+  
+}
+
+// This function reads the current RTC counter value and returns it
+uint32_t rtcGetUnixTime(void)
+    /* To-Do: determine what this note in the datasheet means:
+     * The following registers are synchronized when read:
+     *  • The Counter Value register, COUNT, if the Counter Read Sync Enable bit in CTRLA (CTRLA.COUNTSYNC) is '1'
+     */
+{
+  while (RTC->MODE0.SYNCBUSY.bit.COUNT != 0) ;                    // wait for COUNT sync before reading per SAMD5xE5x Family Datasheet...
+  return RTC->MODE0.COUNT.reg;                                              //  then return it.
+}
+
+// This function sets the current RTC counter value
+void rtcSetUnixTime(uint32_t curUnixTime)
+{
+  RTC->MODE0.COUNT.reg = curUnixTime;                                      // set RTC to input value
+  while (RTC->MODE0.SYNCBUSY.bit.COUNT != 0) ;                   // wait for COUNT sync before returning...
+}
+
